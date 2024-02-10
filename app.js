b110e00b806c2584b961ba52e467dec52a4871e3
Marco <bkudra06@gmail.com>
2024-02-10 14:37:45 +0300

    dark theme added

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
-const defaultCity = 'kiev';
-const tempUI = document.getElementById('temperature');
-const locationUI = document.getElementById('location');
-const descUI = document.getElementById('description');
-const getWeatherBtn = document.getElementById('featchWeatherBtn');
-const resetWeatherBtn = document.getElementById('resetWeatherBtn'); 
+const DEFAULT_CITY = 'kiev';
+const TEMP_UI = document.getElementById('temperature');
+const LOCATION_UI = document.getElementById('location');
+const DESC_UI = document.getElementById('description');
+const HEADER = document.getElementById('header');
+const GET_WEATHER_BTN= document.getElementById('featchWeatherBtn');
+const RESET_WEATHER_BTN = document.getElementById('resetWeatherBtn');
+const FAVICON = document.getElementById('favicon'); 
+const SHORTCUT_ICON = document.getElementById('shortcutIcon');
+const THEME_BTN = document.getElementById('themeBtn');
 
-var lastCities = [defaultCity];
-var counter = 1;
+var isDark = false;
 
 function updateUI(data) {
-        locationUI.textContent = data.name;
-        tempUI.textContent = `${Math.round(data.main.temp - 273.15)}°C`;
-        if (Math.round(data.main.temp - 273.15) >= 30) {
-                tempUI.classList.add('hot');
-        }
-        else if (Math.round(data.main.temp - 273.15) >= 25) {
-                tempUI.classList.add('warm');
-        }
-        else if (Math.round(data.main.temp - 273.15) >= 20) {
-                tempUI.classList.add('normal');
-        }
-        else if (Math.round(data.main.temp - 273.15) <= 10) {
-            tempUI.classList.add('cold');       
-        }
-        descUI.textContent = data.weather[0].description;   
+        LOCATION_UI.textContent = data.name;
+        TEMP_UI.textContent = `${Math.round(data.main.temp - 273.15)}°C`;
+        DESC_UI.textContent = data.weather[0].description;   
 }
 
 
 function getWeather(city) {
-    const apiKey = '77da67154ab3412227f3a5dabba692a5';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const API = '77da67154ab3412227f3a5dabba692a5';
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`;
     
-    fetch(apiUrl)
+    fetch(API_URL)
       .then(response => response.json())
       .then(data => {
         updateUI(data);
@@ -43,20 +34,21 @@ function getWeather(city) {
 }
 
 
-getWeather(defaultCity);
+getWeather(DEFAULT_CITY);
 
-getWeatherBtn.addEventListener('click', function () {
+ GET_WEATHER_BTN.addEventListener('click', function () {
     cityInput = prompt('Enter your city, please: ') || defaultCity;
-    lastCities.push(cityInput);
-    counter = counter + 1;
     getWeather(cityInput);
 });
-resetWeatherBtn.addEventListener('click', function () {
-    if (counter <= 0) {
-        getWeather(defaultCity);
+THEME_BTN.addEventListener('click', function() {
+    isDark = !isDark;
+    document.body.classList.toggle('dark', isDark);
+    HEADER.classList.toggle('dark', isDark);
+    if (isDark) {
+        FAVICON.href = '/images/fav/icons8-night-32.png';
+        SHORTCUT_ICON.href = '/images/fav/icons8-night-32.png';
     } else {
-        counter = counter- 1;
-        getWeather(lastCities[counter]);
-        lastCities.pop(lastCities[1]);
+        FAVICON.href = '/images/fav/icons8-partly-cloudy-day-cute-clipart-32.png';
+        SHORTCUT_ICON.href = '/images/fav/icons8-partly-cloudy-day-cute-clipart-32.png';
     }
 })
